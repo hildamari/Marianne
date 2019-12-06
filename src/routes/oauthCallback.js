58fca04a978ca2aa0ea54a192bc9a5e0e16d613f
KunoichiZ <KunoichiZ@users.noreply.github.com>
2019-12-06 15:02:22 -0600

    Add routes

---
+++ src/routes/oauthCallback.js
@@ -0,0 +1,56 @@
+const { Route, util: { encrypt }, constants: { RESPONSES } } = require('klasa-dashboard-hooks');
+const fetch = require('node-fetch');
+
+module.exports = class extends Route {
+
+	constructor(...args) {
+		super(...args, { route: 'oauth/callback' });
+	}
+
+	get oauthUser() {
+		return this.store.get('oauthUser');
+	}
+
+	async post(request, response) {
+		/* eslint-disable camelcase */
+		if (!request.body.code) return this.noCode(response);
+		const url = new URL('https://discordapp.com/api/oauth2/token');
+		url.search = new URLSearchParams([
+			['grant_type', 'authorization_code'],
+			['redirect_uri', `http://localhost:3003/oauth/callback`],
+			['code', request.body.code]
+		]);
+		const res = await fetch(url, {
+			headers: { Authorization: `Basic ${Buffer.from(`${this.client.options.clientID}:${this.client.options.clientSecret}`).toString('base64')}` },
+			method: 'POST'
+		});
+		if (!res.ok) return response.end(RESPONSES.FETCHING_TOKEN);
+
+		const { oauthUser } = this;
+
+		if (!oauthUser) return this.notReady(response);
+
+		const body = await res.json();
+		const user = await oauthUser.api(body.access_token);
+
+		return response.end(JSON.stringify({
+			access_token: encrypt({
+				token: body.access_token,
+				scope: [user.id, ...user.guilds.filter(guild => guild.userCanManage).map(guild => guild.id)]
+			}, this.client.options.clientSecret),
+			user
+		}));
+		/* eslint-enable camelcase */
+	}
+
+	notReady(response) {
+		response.writeHead(500);
+		return response.end(RESPONSES.NOT_READY);
+	}
+
+	noCode(response) {
+		response.writeHead(400);
+		return response.end(RESPONSES.NO_CODE);
+	}
+
+};
