beb793b3f32cce14d21bb5271ba961dd64614de5
KunoichiZ <KunoichiZ@users.noreply.github.com>
2019-12-06 14:59:04 -0600

    Update Marianne.js

--- src/Marianne.js
+++ src/Marianne.js
@@ -1,65 +1,34 @@
-// const path = require('path');
-// const { Client, SyncSQLiteProvider } = require('discord.js-commando');
-// const Database = require('better-sqlite3');
-
-// const client = new Client({
-//     owner: '147800635046232064',
-//     commandPrefix: process.env.PREFIX,
-//     unknownCommandResponse: false,
-//     disableEveryone: true,
-//     invite: 'https://discord.gg/phsGJdh',
-//     selfbot: false,
-//     presence: {
-//       status: 'online',
-//       activity: {
-//           name: '@Marianne help',
-//           type: 'WATCHING'
-//       }
-//     }
-// });
-
-// const database = new Database(path.join(__dirname, 'data/databases/settings.sqlite3'));
-
-// client.setProvider(new SyncSQLiteProvider(database));
-
-// client.registry
-//     .registerDefaultTypes()
-//     .registerGroups([
-//         ['3h', 'three houses commands'],
-//         ['info', 'info commands'],
-//         ['misc', 'misc commands'],
-//         ['owner', 'owner commands']
-//     ])
-//     .registerDefaultGroups()
-//     .registerDefaultCommands({
-//         'help': true,
-//         'prefix': true,
-//         'ping': true,
-//         'eval_': false,
-//         'commandState': true
-//       })
-//     .registerCommandsIn(path.join(__dirname, 'commands'));
-
-// client.on('ready', () => {
-//     console.log('Logged in!');
-// });
-      
-// client.on('error', console.error);
-      
-// client.login(process.env.TOKEN);
-
 const { Client } = require('klasa');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, 'data/.env')});
 
-new Client({
+Client.use(require('klasa-dashboard-hooks'));
+Client.defaultUserSchema.add('TODOs', 'any', { array: true });
+
+const MarianneClient = new Client ({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     fetchAllMembers: false,
+    createPiecesFolders: false,
     prefix: process.env.PREFIX,
     commandEditing: true,
     disableEveryone: true,
     ignoreBots: true,
+    dashboardHooks: {
+        apiPrefix: '/',
+		port: 1123
+    },
+    pieceDefaults: {
+		routes: {
+			enabled: true,
+			authenticated: false
+		},
+		middlewares: { enabled: false }
+	},
+    providers: { default: 'json' },
     presence: { activity: { name: 'Dorte', type: 'LISTENING' } },
     typing: false,
     owners: ['147800635046232064'],
-    readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`
-}).login(process.env.TOKEN);
+    readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`});
+    
+MarianneClient.login(process.env.TOKEN);
