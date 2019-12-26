1f4f0f0c3ed79790d2fcd8fb978360226ca1463e
Kaoru <KunoichiZ@users.noreply.github.com>
2019-12-26 12:21:26 -0600

    Update bio.js

--- src/commands/Three Houses/bio.js
+++ src/commands/Three Houses/bio.js
@@ -18,52 +18,89 @@ module.exports = class extends Command {
         const unitBioEmbed = new MessageEmbed()
 
         let name = '', aliases = '', gender = '', race = '', birthday = '', fodlanBirth = '', relatives = '',
-        crest = '', nationality = '', hometown = '', residence = '', faction = '', occupation = '', startingClass = '', hexColor = '', thumbnail = '', crestImage = '';
+        crest = '', nationality = '', hometown = '', residence = '', faction = '', occupation = '', startingClass = '', hexColor = '', thumbnail = '', crestImage = '', recruitment = [];
 
         switch(unit) {
             // case 'BylethM':
+            // case 'bylethm':
+            // case 'bylethf':
             // case 'BylethF':
+            // case 'byleth':
+            // case 'Byleth':
             // case 'Edelgard':
+            // case 'edelgard':
             // case 'Hubert':
+            // case 'hubert':
             // case 'Ferdinand':
+            // case 'ferdinand':
             // case 'Linhardt':
+            // case 'linhardt':
             // case 'Caspar':
+            // case 'caspar':
             // case 'Bernadetta':
+            // case 'bernadetta':
             // case 'Dorothea':
+            // case 'dorothea':
             // case 'Petra':
+            // case 'petra':
             // case 'Dimitri':
+            // case 'dimitri':
             // case 'Dedue':
+            // case 'dedue':
             // case 'Felix':
+            // case 'felix':
             // case 'Ashe':
+            // case 'ashe':
             // case 'Sylvain':
+            // case 'sylvain':
             // case 'Mercedes':
+            // case 'mercedes':
             // case 'Annette':
+            // case 'annette':
             // case 'Ingrid':
+            // case 'ingrid':
             // case 'Claude':
+            // case 'claude':
             case 'Hilda':
             case 'hilda':
                 name = 'Hilda Valentine Goneril', aliases = 'N/A', gender = 'Female', race = 'Human', birthday = 'February 3rd', fodlanBirth = '3rd of the Pegasus Moon, Year 1162', relatives = 'Father, Holst Goneril, Goneril', crest = 'Minor Crest of Goneril', nationality = 'Leicester', hometown = 'Duchy of Goneril', residence = 'Garreg Mach Monastery', faction = 'Golden Deer', occupation = 'Student at the Officers Academy', startingClass = 'Noble', thumbnail = 'https://i.imgur.com/zoScHRC.png', crestImage = 'https://i.imgur.com/mKK2nws.png'
               break;
             // case 'Lorenz':
+            // case 'lorenz':
             // case 'Raphael':
+            // case 'raphael':
             // case 'Ignatz':
+            // case 'ignatz':
             // case 'Lysithea':
+            // case 'lysithea':
             // case 'Marianne':
+            // case 'marianne':
             // case 'Leonie':
+            // case 'leonie':
             // case 'Flayn':
+            // case 'flayn':
             case 'Seteth':
             case 'seteth':
                 name = 'Seteth', aliases = '||Cichol||', gender = 'Male', race = 'Earth Dragon', birthday = 'December 27', fodlanBirth = '27th of the Ethereal Moon, Year ????', relatives = 'Flayn ||(Cethleann), Macuil, Indech, Rhea (Seiros), Sothis||', crest = 'Major Crest of Cichol', nationality = '||Nabatean||', hometown = '||Zanado||', residence = 'Garreg Mach Monastery', faction = 'Church of Seiros, ||Four Saints||', occupation = 'Aid to Archbishop Rhea, Instructor', startingClass = 'Wyvern Rider', hexColor = '#71bea4', thumbnail = 'https://i.imgur.com/s35oLUZ.png', crestImage = 'https://i.imgur.com/mJsn0KU.png'
               break;
             // case 'Cyril':
+            // case 'cyril':
             // case 'Anna':
+            // case 'anna':
             // case 'Jeritza':
+            // case 'jeritza':
             // case 'Hanneman':
+            // case 'hanneman':
             // case 'Manuela':
+            // case 'manuela':
             // case 'Gilbert':
+            // case 'gilbert':
             // case 'Alois':
+            // case 'alois':
             // case 'Catherine':
+            // case 'catherine':
             // case 'Shamir':
+            // case 'shamir':
           }
 
           unitBioEmbed
