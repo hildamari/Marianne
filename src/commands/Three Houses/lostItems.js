c34b540dff8d692e59eab47f126e931ae158d013
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-15 12:02:45 -0600

    Create lostItems.js

---
+++ src/commands/Three Houses/lostItems.js
@@ -0,0 +1,118 @@
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+
+    constructor(...args) {
+        super(...args, { 
+            description: 'Replies with information about specific units\' lost items from Fire Emblem Three Houses',
+            extendedHelp: 'This command contains spoilers for Fire Emblem Three Houses!',
+            usage: '<unit:string>'
+        });
+
+        this.customizeResponse('unit',
+            'You must enter a valid unit!');
+    }
+
+    verifyUnit(unit) {
+        const unitLostItemsEmbed = new MessageEmbed()
+
+        let name = '', lostItems = [], hexColor = "", thumbnail = "";
+
+        switch(unit) {
+            case 'Edelgard':
+            case 'edelgard':
+                name = 'Edelgard'
+                thumbnail = 'https://i.imgur.com/HQ3yEk4.png'
+                hexColor = '#fefff4'
+                break;
+            // case 'Hubert':
+            // case 'hubert':
+            // case 'Ferdinand':
+            // case 'ferdinand':
+            // case 'Linhardt':
+            // case 'linhardt':
+            // case 'Caspar':
+            // case 'caspar':
+            // case 'Bernadetta':
+            // case 'bernadetta':
+            // case 'Dorothea':
+            // case 'dorothea':
+            // case 'Petra':
+            // case 'petra':
+            // case 'Dimitri':
+            // case 'dimitri':
+            // case 'Dedue':
+            // case 'dedue':
+            // case 'Felix':
+            // case 'felix':
+            // case 'Ashe':
+            // case 'ashe':
+            // case 'Sylvain':
+            // case 'sylvain':
+            // case 'Mercedes':
+            // case 'mercedes':
+            // case 'Annette':
+            // case 'annette':
+            // case 'Ingrid':
+            // case 'ingrid':
+            // case 'Claude':
+            // case 'claude':
+            // case 'Hilda':
+            // case 'hilda':
+            // case 'Lorenz':
+            // case 'lorenz':
+            // case 'Raphael':
+            // case 'raphael':
+            // case 'Ignatz':
+            // case 'ignatz':
+            // case 'Lysithea':
+            // case 'lysithea':
+            // case 'Marianne':
+            // case 'marianne':
+            // case 'Leonie':
+            // case 'leonie':
+            // case 'Flayn':
+            // case 'flayn':
+            // case 'Seteth':
+            // case 'seteth':
+            // case 'Cyril':
+            // case 'cyril':
+            // case 'Anna':
+            // case 'anna':
+            // case 'Jeritza':
+            // case 'jeritza':
+            // case 'Hanneman':
+            // case 'hanneman':
+            // case 'Manuela':
+            // case 'manuela':
+            // case 'Gilbert':
+            // case 'gilbert':
+            // case 'Alois':
+            // case 'alois':
+            // case 'Catherine':
+            // case 'catherine':
+            // case 'Shamir':
+            // case 'shamir':
+            default:
+                name = "N/A"
+        }
+    
+              if(name == "N/A") {
+                  return "Not a playable unit"
+              } else {
+                    unitLostItemsEmbed
+                        .setColor(hexColor)
+                        .setAuthor(name)
+                        .setDescription("Character Recruitment")
+                        .setThumbnail(thumbnail)
+                        .addField("Lost Items", lostItems.join(", "))
+                    return unitLostItemsEmbed
+                }
+    }
+
+    run(msg) {
+        
+    }
+
+};
