c5177fa3d673b5af978ad4ecc2262445d853877c
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-15 23:26:52 -0600

    Add Golden Deer and Blue Lions to growths

--- src/commands/Three Houses/growths.js
+++ src/commands/Three Houses/growths.js
@@ -99,53 +99,119 @@ module.exports = class extends Command {
               break;
             case 'Dimitri':
             case 'dimitri':
-
+              name = 'Dimitri'
+              thumbnail = 'https://i.imgur.com/Ddl1C8a.png'
+              hexColor = '#fcfba8'
+              hp = 55, str = 60, mag = 20, dex = 50, spd = 50, lck = 25, def = 40, res = 20, cha = 55
+              break;
             case 'Dedue':
             case 'dedue':
-
+              name = 'Dedue'
+              thumbnail = 'https://i.imgur.com/quIqslt.png'
+              hexColor = '#e8e9e2'
+              hp = 60, str = 50, mag = 15, dex = 30, spd = 20, lck = 25, def = 50, res = 10, cha = 30
+              break;
             case 'Felix':
             case 'felix':
-
+              name = 'Felix'
+              thumbnail = 'https://i.imgur.com/YJcGEiK.png'
+              hexColor = '#403f64'
+              hp = 45, str = 55, mag = 30, dex = 45, spd = 55, lck = 40, def = 30, res = 20, cha = 30
+              break;
             case 'Ashe':
             case 'ashe':
-
+              name = 'Ashe'
+              thumbnail = 'https://i.imgur.com/L4iJ8Ug.png'
+              hexColor = '#b5bdc6'
+              hp = 35, str = 35, mag = 25, dex = 55, spd = 50, lck = 40, def = 20, res = 35, cha = 25
+              break;
             case 'Sylvain':
             case 'sylvain':
-
+              name = 'Sylvain'
+              thumbnail = 'https://i.imgur.com/77T2SRt.png'
+              hexColor = '#e06c53'
+              hp = 55, str = 45, mag = 30, dex = 35, spd = 50, lck = 35, def = 40, res = 25, cha = 40
+              break;
             case 'Mercedes':
             case 'mercedes':
-
+              name = 'Mercedes'
+              thumbnail = 'https://i.imgur.com/y1BWI7T.png'
+              hexColor = '#f4dcc4'
+              hp = 30, str = 25, mag = 50, dex = 45, spd = 40, lck = 30, def = 25, res = 45, cha = 40
+              break;
             case 'Annette':
             case 'annette':
-
+              name = 'Annette'
+              thumbnail = 'https://i.imgur.com/lnE7TRE.png'
+              hexColor = '#f6a67d'
+              hp = 25, str = 30, mag = 50, dex = 50, spd = 35, lck = 35, def = 20, res = 30, cha = 35
+              break;
             case 'Ingrid':
             case 'ingrid':
-
+              name = 'Ingrid'
+              thumbnail = 'https://i.imgur.com/t8fCo54.png'
+              hexColor = '#f8e5a0'
+              hp = 40, str = 35, mag = 35, dex = 40, spd = 60, lck = 45, def = 30, res = 40, cha = 45
+              break;
             case 'Claude':
             case 'claude':
-
+              name = 'Claude'
+              thumbnail = 'https://i.imgur.com/A4i8b9Z.png'
+              hexColor = '#4a4240'
+              hp = 35, str = 40, mag = 25, dex = 60, spd = 55, lck = 45, def = 30, res = 25, cha = 55
+              break;
             case 'Hilda':
             case 'hilda':
-                
+              name = 'Hilda'
+              thumbnail = 'https://i.imgur.com/zoScHRC.png'
+              hexColor = '#fcafb7'
+              hp = 50, str = 45, mag = 25, dex = 30, spd = 50, lck = 35, def = 35, res = 20, cha = 50
               break;
             case 'Lorenz':
             case 'lorenz':
-
+              name = 'Lorenz'
+              thumbnail = 'https://i.imgur.com/uYopDET.png'
+              hexColor = '#7373ac'
+              hp = 55, str = 40, mag = 40, dex = 45, spd = 40, lck = 25, def = 30, res = 40, cha = 35
+              break;
             case 'Raphael':
             case 'raphael':
-
+              name = 'Raphael'
+              thumbnail = 'https://i.imgur.com/MB3wRvI.png'
+              hexColor = '#f0d9a1'
+              hp = 65, str = 50, mag = 15, dex = 35, spd = 15, lck = 35, def = 45, res = 10, cha = 25
+              break;
             case 'Ignatz':
             case 'ignatz':
-
+              name = 'Ignatz'
+                thumbnail = 'https://i.imgur.com/TUREZkF.png'
+                hexColor = '#c6c69d'
+              hp = 35, str = 35, mag = 30, dex = 50, spd = 50, lck = 55, def = 25, res = 35, cha = 25
+              break;
             case 'Lysithea':
             case 'lysithea':
-
+              name = 'Lysithea'
+              thumbnail = 'https://i.imgur.com/EIm3Zof.png'
+              hexColor = '#fefcfd'
+              hp = 20, str = 15, mag = 60, dex = 60, spd = 50, lck = 15, def = 10, res = 25, cha = 25
+              break;
             case 'Marianne':
             case 'marianne':
+              name = 'Marianne'
+              thumbnail = 'https://i.imgur.com/OyUl9he.png'
+              hexColor = '#aad0fa'
+              hp = 35, str = 20, mag = 50, dex = 40, spd = 40, lck = 35, def = 15, res = 45, cha = 45
+              break;
             case 'Leonie':
             case 'leonie':
+              name = 'Leonie'
+              thumbnail = 'https://i.imgur.com/yZxvJG4.png'
+              hexColor = '#f17d5b'
+              hp = 40, str = 40, mag = 20, dex = 55, spd = 60, lck = 40, def = 40, res = 15, cha = 40
+              break;
             case 'Flayn':
             case 'flayn':
+
             case 'Seteth':
             case 'seteth':
                 
