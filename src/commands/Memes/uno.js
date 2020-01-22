231033504c162a9c4c6ff494a1c36e15f3d24633
Kaoru <KunoichiZ@users.noreply.github.com>
2020-01-22 12:34:35 -0600

    Update uno.js

--- src/commands/Memes/uno.js
+++ src/commands/Memes/uno.js
@@ -9,11 +9,11 @@ module.exports = class extends Command {
     run(msg) {
         const marianne = this.client.emojis.find(emoji => emoji.name === "Marianne");
         if(msg.guild.id == 649054519556308992) {
-            if(msg.channel.id != 649063924607614978) {
-                msg.send("You cannot use this command in a non-nsfw channel! Please go to <#649063924607614978>")
-            } else {
+            if(msg.channel.id == 669560106614259722 || msg.channel.id == 649063924607614978) {
                 const message = `${marianne} I don't have uno so go fuck off.`
                 return msg.send(message);
+            } else {
+                msg.send("You cannot use this command in a non-nsfw channel! Please go to <#649063924607614978>")
             }
         } else {
             const message = `${marianne} I don't have uno so go fuck off.`
