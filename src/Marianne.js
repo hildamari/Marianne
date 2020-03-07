728fa9434c3dc492b193ce6c9235faa8378d7145
Kaoru <KunoichiZ@users.noreply.github.com>
2020-03-06 23:06:06 -0600

    add dblapi.js

--- src/Marianne.js
+++ src/Marianne.js
@@ -1,6 +1,7 @@
 const { Client } = require('klasa');
 const path = require('path');
 require('dotenv').config({path: path.join(__dirname, 'data/.env')});
+const DBL = require("dblapi.js");
 
 Client.use(require('klasa-dashboard-hooks'));
 Client.defaultUserSchema.add('TODOs', 'any', { array: true });
@@ -25,4 +26,16 @@ const MarianneClient = new Client ({
     owner: '147800635046232064',
     readyMessage: (client) => `Successfully initialized. Ready to serve ${client.guilds.size} guilds.`});
     
+const dbl = new DBL(process.env.DBL_API, MarianneClient);
+
+MarianneClient.on('ready', () => {
+    setInterval(() => {
+        dbl.postStats(MarianneClient.guilds.size);
+    }, 1800000);
+});
+    
+dbl.on('posted', () => {
+    console.log('Server count posted!');
+})
+
 MarianneClient.login(process.env.TOKEN);
