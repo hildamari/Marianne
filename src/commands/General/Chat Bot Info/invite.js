9498834b778bb92068a1c60f7bca6bc5a836ead5
Kaoru <KunoichiZ@users.noreply.github.com>
2020-05-19 12:10:34 -0500

    Update invite.js

--- src/commands/General/Chat Bot Info/invite.js
+++ src/commands/General/Chat Bot Info/invite.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
     }
 
     run(msg) {
-        msg.send("To add me to your discord guild: <https://discordapp.com/oauth2/authorize?client_id=650566385881317376&scope=bot&permissions=322630>\nDon't be afraid to uncheck some permissions, I will let you know if you're trying to run a command without permissions.")
+        msg.send("To add me to your Discord guild: <https://discordapp.com/oauth2/authorize?client_id=650566385881317376&scope=bot&permissions=322630>\nDon't be afraid to uncheck some permissions, I will let you know if you're trying to run a command without permissions.")
     }
 
 }
