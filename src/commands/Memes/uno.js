f4509b419a6433ab0565c50eb07650130f92197e
Kaoru <KunoichiZ@users.noreply.github.com>
2020-05-19 12:04:14 -0500

    Update uno.js

--- src/commands/Memes/uno.js
+++ src/commands/Memes/uno.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
     run(msg) {
         const marianne = this.client.emojis.find(emoji => emoji.name === "Marianne");
         if(msg.guild.id == 649054519556308992) {
-            if(msg.channel.id == 669560106614259722 || msg.channel.id == 649063924607614978) {
+            if(msg.channel.id == 669560106614259722) {
                 const message = `${marianne} I don't have uno so go fuck off.`
                 return msg.send(message);
             } else {
